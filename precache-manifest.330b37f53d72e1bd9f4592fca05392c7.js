4b75920adf37463415500265a2fd3bcb2e1cd38b
gaverdugo <germanbverdugo@gmail.com>
2019-05-31 09:04:37 -0700

    Updates

---
+++ precache-manifest.330b37f53d72e1bd9f4592fca05392c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e434459808bc6222bd60727a328fdde8",
+    "url": "/react-cafe-memory-game/index.html"
+  },
+  {
+    "revision": "bfa9ef60889b0e2ad720",
+    "url": "/react-cafe-memory-game/static/css/main.5e471a17.chunk.css"
+  },
+  {
+    "revision": "767e158467df4297e433",
+    "url": "/react-cafe-memory-game/static/js/2.8515f2cd.chunk.js"
+  },
+  {
+    "revision": "bfa9ef60889b0e2ad720",
+    "url": "/react-cafe-memory-game/static/js/main.a042024a.chunk.js"
+  },
+  {
+    "revision": "46203f6b00aa7bb78eb5",
+    "url": "/react-cafe-memory-game/static/js/runtime~main.98c1a7e1.js"
+  }
+]);
